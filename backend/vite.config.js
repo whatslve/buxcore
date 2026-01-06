6fdfac19f34aaea3cf193044a3e65b533eb820eb
vlad <whatslve@gmail.com>
2026-01-06 15:48:20 +0300

    vite env changed

--- backend/vite.config.js
+++ backend/vite.config.js
@@ -1,30 +1,36 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-    plugins: [
+export default defineConfig(({ mode }) => {
+    // Загружаем env
+    const env = loadEnv(mode, process.cwd(), '')
 
-        laravel({
-            // CSS тянем импортом в app.js; отдельный input для CSS в dev не нужен
-            input: ['resources/js/app.js'],
-            refresh: true,
-        }),
-        vue(),
-        tailwindcss(),
-    ],
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-        // в dev обычно достаточно CORS: true; отказ соединения — не про CORS
-        cors: true,
-        // важное: HMR указывает реальный IP, по которому к контейнеру коннектится браузер
-        hmr: {
-            host: '192.168.1.117',
-            protocol: 'ws',
-            clientPort: 5173,   // помогает в некоторых сетях/NAT
+    const DEV_HOST = env.VITE_DEV_HOST || 'localhost'
+    const DEV_PORT = Number(env.VITE_DEV_PORT || 5173)
+
+    return {
+        plugins: [
+            laravel({
+                input: ['resources/js/app.js'],
+                refresh: true,
+            }),
+            vue(),
+            tailwindcss(),
+        ],
+
+        server: {
+            host: '0.0.0.0',
+            port: DEV_PORT,
+            strictPort: true,
+            cors: true,
+
+            hmr: {
+                host: DEV_HOST,
+                protocol: 'ws',
+                clientPort: DEV_PORT,
+            },
         },
-        strictPort: true,     // если порт занят — сразу увидишь понятную ошибку
-    },
+    }
 })
