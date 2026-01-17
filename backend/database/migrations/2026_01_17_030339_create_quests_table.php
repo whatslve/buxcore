42e46445496ba7f7fba07869d75876c83f1e6903
vlad <whatslve@gmail.com>
2026-01-17 08:03:12 +0300

    quests foreign id for user added

--- backend/database/migrations/2026_01_17_030339_create_quests_table.php
+++ backend/database/migrations/2026_01_17_030339_create_quests_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('quests', function (Blueprint $table) {
             $table->id();
-
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('name');
             $table->string('type');
 
